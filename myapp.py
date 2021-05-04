8f41c609099f923ea0fc7fe266f808ec67b4e017
Kirill Borisov <66241092+kiborisov@users.noreply.github.com>
2021-05-04 11:17:24 -0400

    updated myapp date

--- myapp.py
+++ myapp.py
@@ -16,7 +16,7 @@ tickerSymbol = 'TSLA'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-5-04')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.write("""
